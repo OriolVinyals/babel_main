7a5a41644a74112b260a72335bd27f9a5bb58e96
vinyals <vinyals@192.168.1.104>
2013-06-20 18:05:35 -0700

    Worked on reader a little bit...

--- PostingParser.py
+++ PostingParser.py
@@ -4,3 +4,28 @@ Created on Jun 18, 2013
 @author: vinyals
 '''
 
+from external import htkmfc
+import numpy as np
+import gflags
+import csv
+gflags.DEFINE_string("root", "mujamuja",
+                     "The root to the cifar dataset (python format)")
+FLAGS = gflags.FLAGS
+
+class PostingParser:
+    def __init__(self, csvfile):
+        self.dictReader = csv.DictReader(open(csvfile, 'rb'),delimiter = ',', quotechar = '"')
+        self.data = []
+        for row in self.dictReader:
+            self.data.append(row)
+    
+    def GetFields(self):
+        return self.dictReader.fieldnames
+    
+    def num_total(self):
+        return len(self.data)
+
+if __name__ == '__main__':
+    testParser = PostingParser("./data/word.kwlist.alignment.csv")
+    print testParser.GetFields()
+    print testParser.num_total()
