15380bc020843f9465d6442bb82ecf41b78389c7
vinyals <vinyals@vinyals-PC>
2013-07-08 10:38:05 -0700

    3 kinds of features

--- DictionaryLearningLinux.py
+++ DictionaryLearningLinux.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     #Xp_cheat = np.asmatrix(babel.labels().astype(np.int)).T
 
     '''Building appended features'''
-    Xtrain = np.hstack((Xp_a1,Xp_score))
+    Xtrain = np.hstack((Xp_a1,Xp_entropy,Xp_score))
     Ytrain = babel.labels().astype(np.int)
     m, std = classifier.feature_meanstd(Xtrain)
     Xtrain -= m
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     Xp_t_a1 = conv.process_dataset(babel_eval, as_2d = True)
     Xp_t_entropy = np.asmatrix(babel_eval_post._entropy).T
     Xp_t_score = np.asmatrix(babel_eval._features).T
-    Xtest = np.hstack((Xp_t_a1,Xp_t_score))
+    Xtest = np.hstack((Xp_t_a1,Xp_t_entropy,Xp_t_score))
     Ytest = babel_eval.labels().astype(np.int)
     Xtest -= m
     Xtest /= std
