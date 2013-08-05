860379444f9e54907564415302e24a25d828954b
vinyals <vinyals@192.168.6.154>
2013-08-05 14:57:43 -0700

    cosmetic

--- DictionaryLearningLinuxDumpScores.py
+++ DictionaryLearningLinuxDumpScores.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     babel_lat = BabelDataset.BabelDataset(list_file, feat_range, posting_file, perc_pos, keep_full_utt=True, 
                                           posting_sampler=babel.posting_sampler,min_dur=min_dur,reader_type='lattice')
     
-    #reassign utterances
+    #reassign utterances (hack because the scp files are wrong)
     babel_post.utt_reader.list_times_utt = babel_lat.utt_reader.list_times_utt
 
     
@@ -116,41 +116,3 @@ if __name__ == '__main__':
     
     kws_scorer.get_score('./data/eval.localutt.xml')
     kws_scorer.get_score('./data/eval.rawscore.xml')
-    
-#     score_file = '/u/vinyals/projects/swordfish/src/python/babel_main/data/eval.localutt.xml'
-# 
-#     thresh_bin = '/u/swegmann/work/std/kws/score_norm/thresh.pl'
-#     gt_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-evalpart1.ecf.xml'
-#     decision_out_file = '/u/vinyals/projects/swordfish/src/python/babel_main/data/eval.localutt.decision.xml'
-#     cmd = thresh_bin + ' ' + gt_file + ' ' + score_file + ' > ' + decision_out_file
-#     print 'Running ',cmd
-#     os.system(cmd)
-#     
-#     eval_bin = 'perl5.14.2 /u/drspeech/projects/swordfish/ThirdParty/F4DE/bin/KWSEval'
-#     gt_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-evalpart1/babel104b-v0.4bY_conv-evalpart1.scoring.ecf.xml'
-#     rttm_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-evalpart1/babel104b-v0.4bY_conv-evalpart1.mitllfa3.rttm'
-#     t_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-eval.kwlist2.xml'
-#     opts = '-o -b -O -B -c'
-#     out_dir = '/u/vinyals/projects/swordfish/src/python/babel_main/data/eval.localutt.decision'
-#     cmd = eval_bin + ' -e ' + gt_file + ' -r ' + rttm_file + ' -t ' + t_file + ' ' + opts + ' ' + ' -f ' + out_dir + ' -s ' + decision_out_file
-#     print 'Running ',cmd
-#     os.system(cmd)
-#     
-#     score_file = '/u/vinyals/projects/swordfish/src/python/babel_main/data/eval.rawscore.xml'
-#   
-#     gt_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-evalpart1.ecf.xml'
-#     decision_out_file = '/u/vinyals/projects/swordfish/src/python/babel_main/data/eval.rawscore.decision.xml'
-#     cmd = thresh_bin + ' ' + gt_file + ' ' + score_file + ' > ' + decision_out_file
-#     print 'Running ',cmd
-#     os.system(cmd)    
-#     
-#     gt_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-evalpart1/babel104b-v0.4bY_conv-evalpart1.scoring.ecf.xml'
-#     rttm_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-evalpart1/babel104b-v0.4bY_conv-evalpart1.mitllfa3.rttm'
-#     t_file = '/u/drspeech/projects/swordfish/IndusDB/IndusDB.latest/babel104b-v0.4bY_conv-eval.kwlist2.xml'
-#     opts = '-o -b -O -B -c'
-#     out_dir = '/u/vinyals/projects/swordfish/src/python/babel_main/data/eval.rawscore.decision'
-#     cmd = eval_bin + ' -e ' + gt_file + ' -r ' + rttm_file + ' -t ' + t_file + ' ' + opts + ' ' + ' -f ' + out_dir + ' -s ' + decision_out_file
-#     print 'Running ',cmd
-#     os.system(cmd)
-    
-    
