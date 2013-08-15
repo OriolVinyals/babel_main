e21b7f43ce92b24682dbe14fc6ffc650e29cf748
vinyals <vinyals@192.168.1.101>
2013-08-15 01:04:06 -0700

    fast networks on

--- Classifier.py
+++ Classifier.py
@@ -31,7 +31,7 @@ class Classifier:
             for i in range(Xtrain_feats.shape[0]):
                 DS.addSample( Xtrain_feats[i,:], [self._Ytrain[i]] )
             DS._convertToOneOfMany()
-            self._nn = buildNetwork(DS.indim, 10, DS.outdim, outclass=SoftmaxLayer)
+            self._nn = buildNetwork(DS.indim, 10, DS.outdim, outclass=SoftmaxLayer, fast=True)
             #n._setParameters([0.0,0.0,0.0,0.0])
             self._nn_trainer = BackpropTrainer( self._nn, dataset=DS, momentum=0.1, verbose=True, weightdecay=gamma, learningrate=0.01, lrdecay=1.0)
             self._nn_trainer.trainOnDataset(DS,epochs=5)
