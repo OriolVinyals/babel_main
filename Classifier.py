5ada520260677fa7d5079c3095acac9af9a4fc51
vinyals <vinyals@192.168.1.101>
2013-08-29 10:31:30 -0700

    mujamuja

--- Classifier.py
+++ Classifier.py
@@ -248,7 +248,9 @@ def Train_atwv(Xtrain_feats,class_instance=None,weight=None,special_bias=None,ad
                         lambda x: f_atwv(x, Xtrain_feats,class_instance,special_bias,add_bias,method,factor,0,gamma)[1],
                         weight)
     #weight = sgd(f_atwv,weight,args=(Xtrain_feats,class_instance,special_bias,add_bias,method,factor,10),disp=True)[0]
-    weight = optimize.fmin_l_bfgs_b(f_atwv,weight,args=(Xtrain_feats,class_instance,special_bias,add_bias,method,factor,0,gamma),disp=True)[0]
+    callback_f = lambda x: f_atwv(x, Xtrain_feats,class_instance,special_bias,add_bias,'exact',0,0,0)[0]
+    callback_f = lambda x: 0
+    weight = optimize.fmin_l_bfgs_b(f_atwv,weight,args=(Xtrain_feats,class_instance,special_bias,add_bias,method,factor,0,gamma),disp=True,callback=callback_f)[0]
     w = weight[: K * dim].reshape(dim, K)
     b = weight[K * dim :]
     return w,b
